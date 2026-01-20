79390174f5e24e7571c113f308073d99990e1a3a
jiaheng <jhsia95@gmail.com>
2026-01-20 22:06:44 +0800

    update workflow

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@
 #
 # engine = create_engine(DATABASE_URL)
 # SessionLocal = sessionmaker(bind=engine)
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
